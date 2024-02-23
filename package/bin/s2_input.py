247bc80e33a24ed3c466d03fc33b5630d6bb4847
ggokdemir <ggokdemir@splunk.com>
2024-02-23 18:02:42 +0100

    Exception handling is done.

--- package/bin/s2_input.py
+++ package/bin/s2_input.py
@@ -14,6 +14,15 @@ from solnlib import log
 from solnlib.modular_input import checkpointer
 from splunktaucclib.modinput_wrapper import base_modinput  as base_mi 
 
+#TODO: Remove after troubleshoot.
+import sys, os
+sys.path.append(os.path.join(os.environ['SPLUNK_HOME'],'etc','apps','SA-VSCode','bin'))
+import splunk_debug as dbg
+dbg.enable_debugging(timeout=25)
+#TODO: Remove debug libraries.
+
+import xml.etree.ElementTree as ET
+
 bin_dir  = os.path.basename(__file__)
 app_name = os.path.basename(os.path.dirname(os.getcwd()))
 
@@ -86,192 +95,166 @@ class ModInputS2_INPUT(base_mi.BaseModInput):
 
     def collect_events(helper, ew):
 
-        headers = ""
-        SessionId = ""
-        Username = helper.get_arg("Username")
-        Password = helper.get_arg("Password")
-        SessionId = helper.get_arg("SessionId")
-        url = helper.get_arg("Server_URL")
-        helper.log_info("\n\n [INFO] Settings for the Add-on : ["+Username+":"+Password+"@"+url+"] \n\n")
+        index=helper.get_arg("index")
+        url = "http://{{Server_URL}}/appdevent/nbapi/event"
+        sessionid_url = "http://{{Server_URL}}/appdevent/nbapi/sessionid"
 
         # insert input values into the url and/or header (helper class handles credential store)
-        opt_account = helper.get_arg('account')
-        
+        opt_SessionId = ""
         opt_Server_URL = helper.get_arg('Server_URL')
-        
         opt_Username = helper.get_arg('Username')
-
         opt_Password = helper.get_arg('Password')
-
         opt_SessionId = helper.get_arg('SessionId')
-        url = url.replace("{{"+'SessionId'+"}}",opt_SessionId)
-        headers = headers.replace("{{"+'SessionId'+"}}",opt_SessionId)
-        
-        if(SessionId != ""):
-            helper.log_info("\n\n [INFO] SessionId for the Add-on : ["+SessionId+"@"+url+"] \n\n")
+
+        helper.log_info("\n\n [INFO] Settings for the "+opt_Server_URL+" : ["+opt_Username+":"+opt_Password+"] \n\n")
+
+        if(opt_SessionId != ""):
+            helper.log_info("\n\n [INFO] SessionId for the Add-on : ["+opt_SessionId+"] \n\n")
+
+            url = url.replace("{{"+'Server_URL'+"}}",opt_Server_URL)
+
+            cookies = {".sessionId": opt_SessionId}
+
+            headers = {"Content-Type": "application/xml"}
+
             try:
-                result_SessionId = SessionId.group(1)
-                helper.log_info("\n\n [INFO] SessionId : {}".format(result_SessionId) +" [Username : "+Username+"] \n\n")
-                
                 # Now execute the api call with the SessionId
 
-                opt_SessionId = helper.get_arg('SessionId')
-                url = url.replace("{{"+'SessionId'+"}}",opt_SessionId)
-                headers = headers.replace("{{"+'SessionId'+"}}",opt_SessionId)
-
-                headers=json.loads(headers)
-                response = helper.send_http_request(url, "GET", headers=headers,  parameters="", payload=None, cookies=None, verify=True, cert=None, timeout=None, use_proxy=True)
+                response = helper.send_http_request(url, "GET", headers=headers,  parameters="", payload=None, cookies=cookies, verify=True, cert=None, timeout=None, use_proxy=True)
 
                 try:
                     response.raise_for_status()
                     
-                except:
-                    helper.log_error ("\n\n [ERROR] "+response.text+"[Username : "+Username+"] \n\n")
+                except Exception as e:
+                    helper.log_error ("\n\n [ERROR] "+response.text+" "+str(e)+" [Username : "+opt_Username+"] \n\n")
                 
                 if (response.status_code == 200):
-                    try:
-                        root = ET.fromstring(data)
-                        def xml_to_dict(item):
-                            if len(item) == 0:
-                                return item.text
-                            result = {}
-                            for i in item:
-                                i_data = xml_to_dict(i)
-                                if i.tag in result:
-                                    if type(result[i.tag]) is list:
-                                        result[i.tag].append(i_data)
-                                    else:
-                                        result[i.tag] = [result[i.tag], i_data]
-                                else:
-                                    result[i.tag] = i_data
-                            return result
-                        xml_dict = {root.tag: xml_to_dict(root)}
-                        # Convert the Python dictionary to JSON
-                        json_data = json.dumps(xml_dict, indent=4)
 
-                        try:
-                            sourcetype=  Username  + "://" + helper.get_input_stanza_names()
-                            event = helper.new_event(source=Username, index=index, sourcetype=sourcetype , data=json_data)
-                            ew.write_event(event)
-                            helper.log_info("\n\n [INFO] Event Inserted in JSON format. \n source="+Username+", index="+index+", sourcetype="+sourcetype+" , data="+json_data+" [Username : "+Username+"] \n\n")
-                        except:
-                            helper.log_error("\n\n [ERROR] Error inserting JSON event. [Username : "+Username+"] \n\n")
-                        
-                    except:
-                        try:
-                            sourcetype=  Username  + "://" + helper.get_input_stanza_names()
-                            event = helper.new_event(source=Username, index=index, sourcetype=sourcetype , data=data)
-                            ew.write_event(event)
-                            helper.log_info("\n\n [INFO] Event Inserted in XML format. \n source="+Username+", index="+index+", sourcetype="+sourcetype+" , data="+data+" [Username : "+Username+"] \n\n")
-                        except:
-                            helper.log_error("\n\n [ERROR] Error inserting XML event. [Username : "+Username+"] \n\n")
+                    try:
+                        #data = json.dumps(response.json())
+                        data=response
+                        index=helper.get_arg("index")
+                        sourcetype=  opt_Username  + "://" + opt_Server_URL
+                        event = helper.new_event(source=opt_Username, index=index, sourcetype=sourcetype , data=data)
+                        ew.write_event(event)
+                        helper.log_info("\n\n [INFO] Event Inserted in XML format. \n source="+opt_Username+", index="+index+", sourcetype="+opt_Server_URL+" , data="+str(data)+" [Username : "+opt_Username+"] \n\n")
+                    except Exception as e:
+                        helper.log_error("\n\n [ERROR] Error inserting XML event. : "+str(e)+" [Username : "+opt_Username+"] \n\n")
 
                 else:
-                    helper.log_info("\n\n [INFO] response.status_code = "+response.status_code+" [Username : "+Username+"] \n\n")
-            except:
-                helper.log_error("\n\n [ERROR] Error using SessionId. [Username : "+Username+"] \n\n")
+                    helper.log_info("\n\n [INFO] response.status_code = "+str(response.status_code)+" [Username : "+opt_Username+"] \n\n")               
+            except Exception as e:
+                helper.log_error("\n\n [ERROR] Error using SessionId. : "+str(e)+" [Username : "+opt_Username+"] \n\n")
 
         else:
+            helper.log_info("\n\n [INFO] No SessionId found in the Input : ["+opt_SessionId+"] \n\n")
+
+            sessionid_url = sessionid_url.replace("{{"+'Server_URL'+"}}",opt_Server_URL)
+
+            headers = {
+                        "Content-Type": "application/xml"
+                    }
+
+            payload = """
+                            <NETBOX-API>
+                                <COMMAND name="Login" num="1" dateformat="tzoffset">
+                                    <PARAMS>
+                                        <USERNAME>{{Username}}</USERNAME>
+                                        <PASSWORD>*{{Password}}*</PASSWORD>
+                                    </PARAMS>
+                                </COMMAND>
+                            </NETBOX-API>
+                        """
+            payload = payload.replace("{{"+'Username'+"}}",opt_Username)
+            payload = payload.replace("{{"+'Password'+"}}",opt_Password)
+
             try:
                 # Now execute the api call if no SessionId is provided.
-                headers=""
-                headers=json.loads(headers)
-                response = helper.send_http_request(url, "GET", headers=headers,  parameters="", payload=None, cookies=None, verify=True, cert=None, timeout=None, use_proxy=True)
 
-                response.raise_for_status()   
+                response = helper.send_http_request(sessionid_url, "GET", headers=headers,  parameters="", payload=payload, cookies=None, verify=True, cert=None, timeout=None, use_proxy=True)
 
+                try:
+                    response.raise_for_status()
+                    
+                except Exception as e:
+                    helper.log_error ("\n\n [ERROR] "+response.text+" "+str(e)+" [Username : "+opt_Username+"] \n\n")
 
                 if response.status_code == 200:
                     try:
-                        data = json.dumps(response.json())
-                        #TODO: find the SessionId in the XML.
+                        #data = json.dumps(response.json())
+                        data = response.content
+
+                        helper.log_info("\n\n [INFO] Response to search for the SessionId. data="+str(data)+" [Username : "+opt_Username+"] \n\n")
 
                         try:
+                            '''
                             # here edit find the <SessionId>
                             tag_start = "<SessionId>"
                             tag_end = "</SessionId>"
                             pattern = f'{re.escape(tag_start)}(.*?)\s*{re.escape(tag_end)}'
                             SessionId = re.search(pattern, data)
+                            '''
+
+                            root = ET.fromstring(data)
+                            SessionId = root.attrib.get('sessionid')
 
                             if(SessionId):
-                                helper.log_info("\n\n [INFO] SessionId for the Add-on : ["+SessionId+"@"+url+"] \n\n")
-                                result_SessionId = SessionId.group(1)
-                                helper.log_info("\n\n [INFO] SessionId : {}".format(result_SessionId) +" [Username : "+Username+"] \n\n")
-                                
-                                # Now execute the api call with the SessionId
+                                opt_SessionId = SessionId
+                                helper.log_info("\n\n [INFO] SessionId for the Add-on : ["+opt_SessionId+"] \n\n")
 
-                                opt_SessionId = helper.get_arg('SessionId')
-                                url = url.replace("{{"+'SessionId'+"}}",opt_SessionId)
-                                headers = headers.replace("{{"+'SessionId'+"}}",opt_SessionId)
+                                url = url.replace("{{"+'Server_URL'+"}}",opt_Server_URL)
 
-                                headers=json.loads(headers)
-                                response = helper.send_http_request(url, "GET", headers=headers,  parameters="", payload=None, cookies=None, verify=True, cert=None, timeout=None, use_proxy=True)
+                                cookies = {".sessionId": opt_SessionId}
+
+                                headers = {"Content-Type": "application/xml"}
 
                                 try:
-                                    response.raise_for_status()
-                                    
-                                except:
-                                    helper.log_error ("\n\n [ERROR] "+response.text+"[Username : "+Username+"] \n\n")
-                                
-                                if response.status_code == 200:
-                                
+                                    # Now execute the api call with the SessionId
+
+                                    response = helper.send_http_request(url, "GET", headers=headers,  parameters="", payload=None, cookies=cookies, verify=True, cert=None, timeout=None, use_proxy=True)
+
                                     try:
-                                        root = ET.fromstring(data)
-                                        def xml_to_dict(item):
-                                            if len(item) == 0:
-                                                return item.text
-                                            result = {}
-                                            for i in item:
-                                                i_data = xml_to_dict(i)
-                                                if i.tag in result:
-                                                    if type(result[i.tag]) is list:
-                                                        result[i.tag].append(i_data)
-                                                    else:
-                                                        result[i.tag] = [result[i.tag], i_data]
-                                                else:
-                                                    result[i.tag] = i_data
-                                            return result
-                                        xml_dict = {root.tag: xml_to_dict(root)}
-                                        # Convert the Python dictionary to JSON
-                                        json_data = json.dumps(xml_dict, indent=4)
-
-                                        try:
-                                            sourcetype=  Username  + "://" + helper.get_input_stanza_names()
-                                            event = helper.new_event(source=Username, index=index, sourcetype=sourcetype , data=json_data)
-                                            ew.write_event(event)
-                                            helper.log_info("\n\n [INFO] Event Inserted in JSON format. \n source="+Username+", index="+index+", sourcetype="+sourcetype+" , data="+json_data+" [Username : "+Username+"] \n\n")
-                                        except:
-                                            helper.log_error("\n\n [ERROR] Error inserting JSON event. [Username : "+Username+"] \n\n")
+                                        response.raise_for_status()
                                         
-                                    except:
-                                        try:
-                                            sourcetype=  Username  + "://" + helper.get_input_stanza_names()
-                                            event = helper.new_event(source=Username, index=index, sourcetype=sourcetype , data=data)
-                                            ew.write_event(event)
-                                            helper.log_info("\n\n [INFO] Event Inserted in XML format. \n source="+Username+", index="+index+", sourcetype="+sourcetype+" , data="+data+" [Username : "+Username+"] \n\n")
-                                        except:
-                                            helper.log_error("\n\n [ERROR] Error inserting XML event. [Username : "+Username+"] \n\n")
+                                    except Exception as e:
+                                        helper.log_error ("\n\n [ERROR] "+response.text+" "+str(e)+" [Username : "+opt_Username+"] \n\n")
+                                    
+                                    if (response.status_code == 200):
 
-                                else:
-                                    helper.log_info("\n\n [INFO] response.status_code = "+response.status_code+" [Username : "+Username+"] \n\n")
-                        except:
-                            helper.log_error("\n\n [ERROR] Error using SessionId. [Username : "+Username+"] \n\n")
-                    except:
-                        helper.log_error("\n\n [ERROR] Error finding SessionId [Username : "+Username+"] \n\n")
+                                        try:
+                                            #data = json.dumps(response.json())
+                                            data = response.content
+                                            
+                                            sourcetype=  opt_Username  + "://" + opt_Server_URL
+                                            event = helper.new_event(source=opt_Username, index=index, sourcetype=sourcetype , data=data)
+                                            helper.log_info("\n\n [INFO] Event to insert in XML format. \n source="+opt_Username+", index="+index+", sourcetype="+opt_Server_URL+" , data="+str(data)+" [Username : "+opt_Username+"] \n\n")
+                                            ew.write_event(event)
+                                            helper.log_info("\n\n [INFO] Event Inserted in XML format. \n source="+opt_Username+", index="+index+", sourcetype="+opt_Server_URL+" , data="+str(data)+" [Username : "+opt_Username+"] \n\n")
+                                        except Exception as e:
+                                            helper.log_error("\n\n [ERROR] Error inserting XML event. : "+str(e)+" [Username : "+opt_Username+"] \n\n")
+
+                                    else:
+                                        helper.log_info("\n\n [INFO] response.status_code = "+str(response.status_code)+" [Username : "+opt_Username+"] \n\n")               
+                                except Exception as e:
+                                    helper.log_error("\n\n [ERROR] Error using SessionId. : "+str(e)+" [Username : "+opt_Username+"] \n\n")
+
+
+                        except Exception as e:
+                            helper.log_error("\n\n [ERROR] Error using SessionId. : "+str(e)+"  [Username : "+opt_Username+"] \n\n")
+                    except Exception as e:
+                        helper.log_error("\n\n [ERROR] Error finding SessionId  : "+str(e)+" [Username : "+opt_Username+"] \n\n")
                 else:
-                    helper.log_info("\n\n [INFO] response.status_code = "+response.status_code+" [Username : "+Username+"] \n\n")
-            except:
-                helper.log_error ("\n\n [ERROR] When executing the api call to get SessionId. [Username : "+Username+"] \n\n")
+                    helper.log_info("\n\n [INFO] response.status_code = "+str(response.status_code)+" [Username : "+opt_Username+"] \n\n")
+            except Exception as e:
+                helper.log_error ("\n\n [ERROR] When executing the api call to get SessionId. : "+str(e)+" [Username : "+opt_Username+"] \n\n")
 
     def get_account_fields(self):
         account_fields = []
         return account_fields
 
-
     def get_checkbox_fields(self):
         checkbox_fields = []
         return checkbox_fields
-
 
     def get_global_checkbox_fields(self):
         if self.global_checkbox_fields is None:
@@ -286,7 +269,6 @@ class ModInputS2_INPUT(base_mi.BaseModInput):
                 self.log_error('Get exception when loading global checkbox parameter names. ' + str(e))
                 self.global_checkbox_fields = []
         return self.global_checkbox_fields
-
 
 if __name__ == '__main__':
     exit_code = ModInputS2_INPUT().run(sys.argv)
